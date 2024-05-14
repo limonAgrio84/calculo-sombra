40499b9af916badc23d4661c4e2f0bdb13858355
Limon <151796984+limonAgrio84@users.noreply.github.com>
2024-05-13 19:34:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const heightInput = document.getElementById("height");
+const sunRiseInput = document.getElementById("sunRise");
+const resultInput = document.getElementById("result");
+const calculate = document.getElementById("calculate");
+const credits = document.getElementById("credits");
+
+calculate.addEventListener("click",()=>{
+    let height = parseFloat(heightInput.value);
+    let sunRise = parseFloat(sunRiseInput.value);
+    let angleInRadians = sunRise * (Math.PI / 180);
+    let tan = Math.tan(angleInRadians);
+    let result = height/tan;
+    resultInput.textContent = "La longitud de la sombra es de "+result+"metros";
+});
+
+credits.addEventListener("click", () => {
+    window.open("https://www.sunearthtools.com/dp/tools/pos_sun.php?lang=es", "_blank");
+});
